a0a870d10ed32aa415fb74d27076c0229245fd9e
vinibartoli <vinibartoli04@gmail.com>
2023-11-14 23:02:20 -0300

    first commit

---
+++ src/main/java/com/bartoli/CursoFaculdadeApiApplication.java
@@ -0,0 +1,13 @@
+package com.bartoli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoFaculdadeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoFaculdadeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bartoli/CursoFaculdadeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bartoli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoFaculdadeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
